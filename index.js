7075378cdfbd0ab34540f2002763f4ab2d35295b
Shubham Singh Thakur <stfushubham@gmail.com>
2022-10-21 17:32:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const readline = require("readline-sync");
+
+EX 01-04
+const userName = readline.question("Aapka kya naam hai? \nHumse kya kaam hai? \n");
+
+let userNameArray = userName.split(" ");
+
+console.log("KBC mein aapka swagat hai " + userNameArray[0] +
+ "! \nTanay Bhaiya jald hi aapko " + userNameArray[4] + " sikha denge :D");
